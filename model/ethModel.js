8c94c67d0859e7727d2a8dd32b2e16edea399583
王友全 <798725792@qq.com>
2021-01-04 11:37:22 +0800

    修改

--- model/ethModel.js
+++ model/ethModel.js
@@ -1,4 +1,4 @@
-const ethers = require('ethers')
+const Web3 = require('web3')
 const fs = require('fs')
 
 class EthModel {
@@ -7,29 +7,28 @@ class EthModel {
     this.initWS()
   }
   initWS () {
-    // this.provider = new ethers.providers.JsonRpcProvider('http://192.168.1.46:8545');
-    this.provider = new ethers.providers.InfuraProvider('ropsten', '68715df044514d4eb102c4abdcdb9a68');
-    // this.getBlockNumber()
+    this.web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/68715df044514d4eb102c4abdcdb9a68'));
+    this.getBlockNumber()
     // this.getGasPrice()
     this.getBlock()
     // this.getBalance()
   }
   async getBlockNumber () {
-    let blockNumber = await this.provider.getBlockNumber()
+    let blockNumber = await this.web3.eth.getBlockNumber()
     console.log(blockNumber)
     return blockNumber
   }
   async getGasPrice () {
-    let gasPrice = await this.provider.getGasPrice()
+    let gasPrice = await this.web3.getGasPrice()
     let gasPriceString = ethers.utils.formatUnits(gasPrice.toString(), 'gwei')
     return gasPriceString
   }
   async getBalance () {
-    let balance = await this.provider.getBalance('ds56b56C2efEf8233dbDd6b5Cae27E4b14036Be6b0')
+    let balance = await this.web3.getBalance('ds56b56C2efEf8233dbDd6b5Cae27E4b14036Be6b0')
     console.log(balance.toString())
   }
   async getBlock () {
-    let blockInfo = await this.provider.getBlock(9392255)
+    let blockInfo = await this.web3.eth.getBlock(9392795)
     console.log(blockInfo)
   }
 }
